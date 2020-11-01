eecf62f89291367eaed3460a317990494d5eeffd
PREACHER <44905222+PRE4CHER@users.noreply.github.com>
2020-11-01 15:14:00 +0300

    Employer class

--- WFA .NET Lab1/Employer.cs
+++ WFA .NET Lab1/Employer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,5 +24,60 @@ namespace WFA.NET_Lab1
             stature = Stature;
             staff = Staff;
         }
+
+        void BinWriter(List<Employer> employers)
+        {
+            try
+            {
+                if (employers != null)
+                {
+                    // создаем объект BinaryWriter
+                    using (BinaryWriter writer = new BinaryWriter(File.Open(Form1::BrowseFolder(), FileMode.OpenOrCreate)))
+                    {
+                        // записываем в файл значение каждого поля структуры
+                        foreach (Employer emp in employers)
+                        {
+                            {
+                                writer.Write(emp.name);
+                                writer.Write(emp.lastName);
+                                writer.Write(emp.age);
+                                writer.Write(emp.stature);
+                                writer.Write(emp.staff);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Form1_Load error:" + ex.Message);
+            }
+        }
+
+        void BinWriter(List<Employer> employers)
+        {
+            try
+            {
+                if (employers != null)
+                {
+                    using (BinaryReader reader = new BinaryReader(File.Open(dataFolder, FileMode.Open)))
+                    {
+                        // пока не достигнут конец файла
+                        // считываем каждое значение из файла
+                        while (reader.PeekChar() > -1)
+                        {
+                            string name = reader.ReadString();
+                            string capital = reader.ReadString();
+                            int area = reader.ReadInt32();
+                            double population = reader.ReadDouble();
+
+                            Console.WriteLine("Страна: {0}  столица: {1}  площадь {2} кв. км   численность населения: {3} млн. чел.",
+                                name, capital, area, population);
+                        }
+                    }
+                }
+            }
+        }
     }
-}
+    }
+
--- WFA .NET Lab1/Form1.cs
+++ WFA .NET Lab1/Form1.cs
@@ -28,35 +28,7 @@ namespace WFA.NET_Lab1
         {
             try
             {
-                // создаем объект BinaryWriter
-                using (BinaryWriter writer = new BinaryWriter(File.Open(BrowseFolder(), FileMode.OpenOrCreate)))
-                {
-                    // записываем в файл значение каждого поля структуры
-                    foreach (Employer emp in employers)
-                    {
-                        writer.Write(emp.name);
-                        writer.Write(emp.lastName);
-                        writer.Write(emp.age);
-                        writer.Write(emp.stature);
-                        writer.Write(emp.staff);
-                    }
-                }
-                // создаем объект BinaryReader
-                using (BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open)))
-                {
-                    // пока не достигнут конец файла
-                    // считываем каждое значение из файла
-                    while (reader.PeekChar() > -1)
-                    {
-                        string name = reader.ReadString();
-                        string capital = reader.ReadString();
-                        int area = reader.ReadInt32();
-                        double population = reader.ReadDouble();
-
-                        Console.WriteLine("Страна: {0}  столица: {1}  площадь {2} кв. км   численность населения: {3} млн. чел.",
-                            name, capital, area, population);
-                    }
-                }
+                
             }
             catch (Exception ex)
             {
